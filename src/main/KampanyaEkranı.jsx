6b53c9e29b5ba7ca38ddb51df9e4ccc5979982b2
Neim-Ramazanoglu <mr.ramazanoglu@gmail.com>
2021-12-02 13:28:06 +0300

    last version

--- src/main/KampanyaEkranı.jsx
+++ src/main/KampanyaEkranı.jsx
@@ -9,6 +9,44 @@ import { styled } from '@mui/material/styles';
 import Slider, { SliderThumb } from '@mui/material/Slider';
 import { useState, useEffect } from 'react';
 
+const PrettoSlider = styled(Slider)({
+    color: '#524091',
+    height: 8,
+    '& .MuiSlider-track': {
+        border: 'none',
+    },
+    '& .MuiSlider-thumb': {
+        height: 24,
+        width: 24,
+        backgroundColor: '#fff',
+        border: '2px solid currentColor',
+        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+            boxShadow: 'inherit',
+        },
+        '&:before': {
+            display: 'none',
+        },
+    },
+    '& .MuiSlider-valueLabel': {
+        lineHeight: 1.2,
+        fontSize: 12,
+        background: 'unset',
+        padding: 0,
+        width: 32,
+        height: 32,
+        borderRadius: '50% 50% 50% 0',
+        backgroundColor: '#524091',
+        transformOrigin: 'bottom left',
+        transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+        '&:before': { display: 'none' },
+        '&.MuiSlider-valueLabelOpen': {
+            transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+        },
+        '& > *': {
+            transform: 'rotate(45deg)',
+        },
+    },
+});
 export default function KampanyaEkranı() {
 
     const [denebi, setDenebi] = useState(50)
@@ -16,44 +54,7 @@ export default function KampanyaEkranı() {
         setDenebi(newValue);
     };
 
-    const PrettoSlider = styled(Slider)({
-        color: '#524091',
-        height: 8,
-        '& .MuiSlider-track': {
-            border: 'none',
-        },
-        '& .MuiSlider-thumb': {
-            height: 24,
-            width: 24,
-            backgroundColor: '#fff',
-            border: '2px solid currentColor',
-            '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-                boxShadow: 'inherit',
-            },
-            '&:before': {
-                display: 'none',
-            },
-        },
-        '& .MuiSlider-valueLabel': {
-            lineHeight: 1.2,
-            fontSize: 12,
-            background: 'unset',
-            padding: 0,
-            width: 32,
-            height: 32,
-            borderRadius: '50% 50% 50% 0',
-            backgroundColor: '#524091',
-            transformOrigin: 'bottom left',
-            transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-            '&:before': { display: 'none' },
-            '&.MuiSlider-valueLabelOpen': {
-                transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-            },
-            '& > *': {
-                transform: 'rotate(45deg)',
-            },
-        },
-    });
+
     console.log(denebi);
     return <>
         <div style={{ height: "100vh" }}>
