8b1fd32b7c0b76a76777a56190e9a1a39e22baeb
Neim-Ramazanoglu <mr.ramazanoglu@gmail.com>
2021-12-06 10:22:31 +0300

    kampanya ekranı mobil responsive

--- src/main/KampanyaEkranı.jsx
+++ src/main/KampanyaEkranı.jsx
@@ -47,6 +47,45 @@ const PrettoSlider = styled(Slider)({
         },
     },
 });
+const PrettoSlider2 = styled(Slider)({
+    color: '#524091',
+    height: 6,
+    padding: 0,
+    '& .MuiSlider-track': {
+        border: 'none',
+    },
+    '& .MuiSlider-thumb': {
+        height: 16,
+        width: 16,
+        backgroundColor: '#fff',
+        border: '2px solid currentColor',
+        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+            boxShadow: 'inherit',
+        },
+        '&:before': {
+            display: 'none',
+        },
+    },
+    '& .MuiSlider-valueLabel': {
+        lineHeight: 1.2,
+        fontSize: 12,
+        background: 'unset',
+        padding: 0,
+        width: 32,
+        height: 32,
+        borderRadius: '50% 50% 50% 0',
+        backgroundColor: '#524091',
+        transformOrigin: 'bottom left',
+        transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+        '&:before': { display: 'none' },
+        '&.MuiSlider-valueLabelOpen': {
+            transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+        },
+        '& > *': {
+            transform: 'rotate(45deg)',
+        },
+    },
+});
 export default function KampanyaEkranı() {
 
     const [denebi, setDenebi] = useState(50)
@@ -64,7 +103,7 @@ export default function KampanyaEkranı() {
             <div className="sub-title text-center">
                 Seni burada gördüğümüz için çok sevindik
             </div>
-            <div className="row ">
+            <div className="row not-phone">
                 <div className="col-4" style={{ display: "flex" }}>
                     <div className="phone">
                         <div className="text-center" style={{ fontSize: "5vh" }}>Başlık</div>
@@ -75,7 +114,6 @@ export default function KampanyaEkranı() {
                                 aria-label="pretto slider"
                                 defaultValue={20}
                             />
-
                             <span >Denebi Puanı</span>
                             <PrettoSlider
                                 valueLabelDisplay="auto"
@@ -83,30 +121,25 @@ export default function KampanyaEkranı() {
                                 value={denebi}
                                 onChange={handleChange}
                             />
-
                             <span>Kullanıcı Puanı</span>
                             <PrettoSlider
                                 valueLabelDisplay="auto"
                                 aria-label="pretto slider"
                                 defaultValue={20}
                             />
-
                             <span>Görüntülenme Sayısı</span>
                             <PrettoSlider
                                 valueLabelDisplay="auto"
                                 aria-label="pretto slider"
                                 defaultValue={20}
                             />
-
                         </div>
 
                     </div>
                 </div>
                 <div className="col-4 " style={{ display: "flex" }}>
                     <div style={{ display: "block", marginLeft: "auto", marginRight: "auto" }}>
-                        <IPhone6Plus width={300} orientation='portrait' color='black' buttonProps={{
-                            onClick: () => alert('Home Button Clicked!')
-                        }}>
+                        <IPhone6Plus width={300} orientation='portrait' color='black' >
                             <div style={{ backgroundColor: "white", height: "100%", color: "black" }}>
                                 <div className="text-center">
                                     <img className="emoji" src={(denebi < 21 ? üzgün : denebi < 41 ? mutlu : denebi < 61 ? çokmutlu : emoji)} />
@@ -133,6 +166,78 @@ export default function KampanyaEkranı() {
                         </div>
                     </div>
 
+                </div>
+            </div>
+            <div className="phonescreen">
+                <div style={{ display: "flex", marginBottom: "-10vh" }}>
+                    <div style={{ display: "block", marginLeft: "auto", marginRight: "auto", zIndex: "-1" }}>
+                        <IPhone6Plus width={150} orientation='portrait' color='black' >
+                            <div style={{ backgroundColor: "white", height: "100%", color: "black" }}>
+                                <div className="text-center">
+                                    <img className="emoji" src={(denebi < 21 ? üzgün : denebi < 41 ? mutlu : denebi < 61 ? çokmutlu : emoji)} />
+                                </div>
+                                <div className="text-center" style={{ fontSize: "5vh" }}>2500</div>
+                                <div className="text-center" style={{ fontSize: "2vh", color: "grey" }}>Potansiyel Satış</div>
+
+                            </div>
+                        </IPhone6Plus>
+                    </div>
+                </div>
+                <div className="row" style={{ zIndex: "999" }}>
+                    <div className="col-6" style={{ display: "flex" }}>
+                        <div className="phone">
+                            <div className="text-center" style={{ fontSize: "4vh" }}>Başlık</div>
+                            <div className="container">
+                                <span>Kaç Gün</span>
+                                <PrettoSlider2
+                                    valueLabelDisplay="auto"
+                                    aria-label="pretto slider"
+                                    defaultValue={20}
+                                    size="small"
+                                    aria-label="Small"
+                                />
+
+                                <span >Denebi Puanı</span>
+                                <PrettoSlider2
+                                    valueLabelDisplay="auto"
+                                    aria-label="pretto slider"
+                                    value={denebi}
+                                    onChange={handleChange}
+                                />
+
+                                <span>Kullanıcı Puanı</span>
+                                <PrettoSlider2
+                                    valueLabelDisplay="auto"
+                                    aria-label="pretto slider"
+                                    defaultValue={20}
+                                />
+
+                                <span>Görüntülenme Sayısı</span>
+                                <PrettoSlider2
+                                    valueLabelDisplay="auto"
+                                    aria-label="pretto slider"
+                                    defaultValue={20}
+                                />
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="col-6" style={{ display: "flex" }}>
+                        <div className="phone">
+                            <div className="container" style={{ marginTop: "3vh" }}>
+                                <span >Toplam Görüntülenme</span>
+                                <div className="mb-1" style={{ color: "black", fontSize: "2vh" }}>15780</div>
+                                <span>Toplam Tıklanma</span>
+                                <div className="mb-1" style={{ color: "black", fontSize: "2vh" }}>15780</div>
+                                <span >Satış</span>
+                                <div className="mb-1" style={{ color: "black", fontSize: "2vh" }}>15780</div>
+                                <span >Aksiyon alan kullanıcı</span>
+                                <div className="mb-1" style={{ color: "black", fontSize: "2vh" }}>15780</div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
 
